557de25d7d0792afe4521c3138899c5be12871fb
josehigor27 <josehigor27@gmail.com>
2022-03-13 21:44:07 -0300

    1.0 - Primeira versao - FrontEnd: useState e useEffect

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -3,20 +3,29 @@ import axios from "axios";
 import MovieCard from "components/moviecard";
 import Pagination from "components/pagination";
 import { BASE_URL } from "utils/requests";
+import { useEffect, useState } from 'react';
+import { MoviePage } from "types/movie";
+
 
 function Listing() {
 
-        // forma errada
 
-        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
             .then(response => {
-                console.log(response.data)
+                const data = response.data as MoviePage;
+                console.log(data);
+                setPageNumber(data.number);
             });
+    }, []);
 
-            
 
     return (
+
         <>
+            <p>{pageNumber}</p>
             <Pagination />
             <div>
                 <div className="row">
