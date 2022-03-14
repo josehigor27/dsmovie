b98e5eabb28c1c0b3ae1c6394b0f8f71485b2442
josehigor27 <josehigor27@gmail.com>
2022-03-13 21:24:14 -0300

    1.0 - Priemir

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,8 +1,18 @@
 
+import axios from "axios";
 import MovieCard from "components/moviecard";
 import Pagination from "components/pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+        // forma errada
+
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+            .then(response => {
+                console.log(response.data)
+            });
+
     return (
         <>
             <Pagination />
